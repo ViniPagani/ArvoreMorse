426b95a96533be604de6869a253c9cb0bc2b9970
ViniPagani <135363594+ViniPagani@users.noreply.github.com>
2024-10-01 22:43:49 -0300

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,12 @@
+public class Node {
+    char caractere;
+    Node filhoEsquerdo;
+    Node filhoDireito;
+
+    public Node() {
+        this.caractere = '\0';
+        this.filhoEsquerdo = null;
+        this.filhoDireito = null;
+    }
+}
+
